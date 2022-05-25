009f8f0a4b65d80e363b186f7411ff035df27a1d
Nowocinski <vito24222@gmail.com>
2022-05-25 09:30:36 +0200

    Removed FirstFragment and SecondFragment from project

--- app/src/main/java/com/example/colorpalette/MainActivity.java
+++ app/src/main/java/com/example/colorpalette/MainActivity.java
@@ -35,10 +35,6 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -78,13 +74,6 @@ public class MainActivity extends AppCompatActivity {
     private void addColor() {
         Intent intent = new Intent(MainActivity.this, ColorActivity.class);
         startActivity(intent);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 
     @Override
